d2a951e813fa8b8fa04bc82a0d77b17605f23d60
akashsathe063 <akashsathe063@gmail.com>
2023-05-02 15:16:18 +0530

    listView

---
+++ app/src/main/java/com/udemy/listview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.udemy.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listView :ListView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.country_name)
+
+        var countryList = resources.getStringArray(R.array.countries)
+
+        var arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,countryList)
+
+        listView.adapter = arrayAdapter
+        
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val countryName : String = parent.getItemAtPosition(position).toString()
+
+            Toast.makeText(this, "You selected the $countryName",Toast.LENGTH_LONG ).show()
+        }
+    }
+}
